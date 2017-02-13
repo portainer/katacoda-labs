8ec5ae248eac68e265a9778c36f3de3acd118b46
Ben Hall <ben@benhall.me.uk>
2017-02-13 11:32:13 +0000

    Need to sort this

--- deploying-to-swarm/env-init.sh
+++ deploying-to-swarm/env-init.sh
@@ -1,4 +1,5 @@
-for i in {1..20}; do docker -H [[HOST_IP]]:2345 swarm init && break || sleep 1; done
+sleep 1
+docker -H [[HOST_IP]]:2345 swarm init
 token=$(docker -H [[HOST_IP]]:2345 swarm join-token -q worker)
 docker -H [[HOST2_IP]]:2345 swarm join [[HOST_IP]]:2377 --token $token
 docker -H [[HOST_IP]]:2345 pull portainer/portainer
