858a0285aa2a5b5b96b30cdbf1519f17211ff013
Ben Hall <Ben@Benhall.me.uk>
2017-05-13 09:09:23 -0500

    Update env-init.sh

--- deploying-to-swarm/env-init.sh
+++ deploying-to-swarm/env-init.sh
@@ -2,5 +2,5 @@ sleep 1
 docker -H [[HOST_IP]]:2345 swarm init
 token=$(docker -H [[HOST_IP]]:2345 swarm join-token -q worker)
 docker -H [[HOST2_IP]]:2345 swarm join [[HOST_IP]]:2377 --token $token
-docker -H [[HOST_IP]]:2345 pull portainer/portainer
 ssh root@host01 mkdir -p /host/data
+docker -H [[HOST_IP]]:2345 pull portainer/portainer
